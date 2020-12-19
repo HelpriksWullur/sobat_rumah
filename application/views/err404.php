75053f032e2b103d58a0972f9f10e4e42dba9072
Rara <71311772+hairara@users.noreply.github.com>
2020-12-19 17:25:40 +0700

    Menambahkan err404.php

---
+++ application/views/err404.php
@@ -0,0 +1,19 @@
+<div class="container-fluid">
+  <div class="pl-5 pr-5" style="height: 36em">
+    <h1 class="text-danger">Oops!</h1>
+    <h3 class="text-danger">Maaf, halaman yang Anda cari tidak ditemukan!</h3><br>
+    <h5>Kenapa ini bisa terjadi?</h5>
+    <ul>
+      <li>Alamat yang Anda tuju belum/tidak tersedia</li>
+      <li>Alamat yang Anda tuju telah dihapus</li>
+    </ul>
+    <br>
+    <h5>Apa yang harus saya lakukan?</h5>
+    <ul>
+      <li>Segarkan halaman</li>
+      <li>Periksa kembali alamat yang Anda tuju</li>
+      <li>Kembali ke halaman sebelumnya atau ke halaman home</li>
+      <li>Hubungi pengembang</li>
+    </ul>
+  </div>
+</div>
