ee185b72abd5dd9d54efd5ad022b44897c7e17ff
Helpriks Wullur <wullurhelpriks@gmail.com>
2020-12-19 13:37:16 +0700

    Menambahkan helper url, ModelUser dan ModelProduk

--- application/config/autoload.php
+++ application/config/autoload.php
@@ -89,7 +89,7 @@ $autoload['drivers'] = array();
 |
 |	$autoload['helper'] = array('url', 'file');
 */
-$autoload['helper'] = array();
+$autoload['helper'] = array('url');
 
 /*
 | -------------------------------------------------------------------
@@ -132,4 +132,4 @@ $autoload['language'] = array();
 |
 |	$autoload['model'] = array('first_model' => 'first');
 */
-$autoload['model'] = array();
+$autoload['model'] = array('ModelUser', 'ModelProduk');
