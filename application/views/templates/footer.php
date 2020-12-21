4d35c90a240fd1a6a41e4f5cb5d34726eb50aa59
Han64n1 <42445155+Han64n1@users.noreply.github.com>
2020-12-21 19:48:15 +0700

    Menambahkan template footer

---
+++ application/views/templates/footer.php
@@ -0,0 +1,54 @@
+<!-- Footer -->
+<footer id="footer-wrapper" class="page-footer font-small pt-4" style="background-color: rgb(207, 100, 0)">
+
+  <!-- Footer Links -->
+  <div class="container text-center text-md-left text-white">
+
+    <!-- Grid row -->
+    <div class="row">
+
+      <!-- Grid column -->
+      <div class="col-md-6 mt-md-0 mt-3">
+
+        <!-- Content -->
+        <h5 class="text-uppercase bold font-weight-bold">tentang Kami</h5>
+        <p>Sobat Rumah adalah ecommerce furniture penyedia perabotan, peralatan dan berbagai macam kebutuhan keluarga lainnya. <a href="<?= base_url('Account/about'); ?>">Ingin tahu lebih?</a></p>
+
+
+      </div>
+      <!-- Grid column -->
+
+      <hr class="clearfix w-100 d-md-none pb-3">
+
+      <!-- Grid column -->
+      <div class="col-md-3 mb-md-0 mb-3">
+
+        <!-- Links -->
+        <h5 class="text-uppercase font-weight-bold">Links</h5>
+
+        <ul class="list-unstyled">
+          <li>
+            <a class="text-white" href="<?= base_url('Account/error'); ?>">Privacy Policy</a>
+          </li>
+        </ul>
+
+      </div>
+      <!-- Grid column -->
+
+      <!-- Grid column -->
+
+    </div>
+    <!-- Grid row -->
+
+  </div>
+  <!-- Footer Links -->
+
+  <!-- Copyright -->
+  <div class="fixed-bottom footer-copyright text-center py-3 bg-light shadow text-secondary">
+    Copyright © 2020 Tim 2 Web Programming 2. All Rights Reserved
+  </div>
+  <!-- Copyright -->
+
+</footer>
+<!-- Footer -->
+</div>
