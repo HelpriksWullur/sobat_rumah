47e610587b452506d79bd51c22f01cc4204bb8f2
Helpriks Wullur <wullurhelpriks@gmail.com>
2021-01-05 01:01:42 +0700

    Menghapus link tidak terpakai

--- application/views/templates/header.php
+++ application/views/templates/header.php
@@ -6,7 +6,6 @@ defined('BASEPATH') or exit('No script access allowed');
   <head>
     <meta charset="utf-8">
     <title>Sobat Rumah - <?php echo $judul; ?></title>
-    <link type="text/css" rel="stylesheet" href="<?= base_url('assets/css/style.css'); ?>">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/css/all.min.css" rel="stylesheet">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
     <script src="https://code.jquery.com/jquery-3.5.1.min.js" integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script>
